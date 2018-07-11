11ccf7461587024898f2411f2131f70c96c5fc25
brutal-kitten <olgaprudyus@gmail.com>
2018-07-11 16:59:58 +0200

    feat: add event listener in sw on install event

--- sw.js
+++ sw.js
@@ -1,2 +1,9 @@
+let staticCacheName = 'v1';
 
-console.log('hi4');
+let urlsToCache = ['/', 'js/main.js', 'css/styles.css', 'img/', ''];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(caches.open(staticCacheName).then(function(cache) {
+      cache.addAll(urlsToCache);
+    }));
+});
