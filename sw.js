889e13a230d047d459b896691c96ce0024db3f92
brutal-kitten <olgaprudyus@gmail.com>
2018-07-12 16:49:27 +0200

    feat:  sw caches all the pages visited online

--- sw.js
+++ sw.js
@@ -7,3 +7,47 @@ self.addEventListener('install', function(event) {
       cache.addAll(urlsToCache);
     }));
 });
+
+function noConnectionResponse(event) {
+  return new Response(caches.match(event.request).then(function(response) {
+    if (response) {
+      return response;
+    } else {
+      return new Response("Sorry, not found");
+      }
+  }));
+}
+
+self.addEventListener('fetch', function (event) {
+  console.log('here');
+  let requestToFetch = event.request.clone();
+  event.respondWith(
+    fetch(requestToFetch).then(function (response) {
+      if(response.status != 400) {
+        let responseToCache = response.clone();
+        caches.open(staticCacheName).then(function(cache){
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      };
+    })
+  );
+});
+
+/**
+  event.respondWith(
+  //  fetch(event.request).then(function (response) {
+    //  if(response.status == 404) {
+    //    noConnectionResponse(event);
+//} else {
+          event.waitUntil(caches.open(staticCacheName).then(function(cache) {
+            cache.put(event.request, response.clone());
+            return response;
+        }))
+      )
+    // }
+    }
+  )
+//  );
+//});
+**/
