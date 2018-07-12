82e40408c199bae2e8c97e76d25f7950c23cbbab
brutal-kitten <olgaprudyus@gmail.com>
2018-07-12 18:46:48 +0200

    feat: sw serves pages from cache when offline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let staticCacheName = 'v1';
+let staticCacheName = 'v2';
 
 let urlsToCache = ['/', 'js/main.js', 'css/styles.css', 'img/', ''];
 
@@ -8,46 +8,23 @@ self.addEventListener('install', function(event) {
     }));
 });
 
-function noConnectionResponse(event) {
-  return new Response(caches.match(event.request).then(function(response) {
-    if (response) {
-      return response;
-    } else {
-      return new Response("Sorry, not found");
-      }
-  }));
-}
-
 self.addEventListener('fetch', function (event) {
   console.log('here');
   let requestToFetch = event.request.clone();
   event.respondWith(
     fetch(requestToFetch).then(function (response) {
-      if(response.status != 400) {
+      if(response.status != 400 && (response)) {
         let responseToCache = response.clone();
         caches.open(staticCacheName).then(function(cache){
           cache.put(event.request, responseToCache);
         });
         return response;
-      };
-    })
+      } else {
+         return new Response("Sorry, not found");
+       };
+     }).catch(function() {
+       return caches.match(event.request);
+     })
+
   );
 });
-
-/**
-  event.respondWith(
-  //  fetch(event.request).then(function (response) {
-    //  if(response.status == 404) {
-    //    noConnectionResponse(event);
-//} else {
-          event.waitUntil(caches.open(staticCacheName).then(function(cache) {
-            cache.put(event.request, response.clone());
-            return response;
-        }))
-      )
-    // }
-    }
-  )
-//  );
-//});
-**/
