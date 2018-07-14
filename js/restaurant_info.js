175193a7ffc9c618a648a27b5c94b06f9a246d35
brutal-kitten <olgaprudyus@gmail.com>
2018-07-14 20:17:18 +0200

    fix: finish madia query for the smallest screen for restourant page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -117,15 +117,18 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
-  const name = document.createElement('p');
+  const name = document.createElement('div');
+  name.className = "name";
   name.innerHTML = review.name;
   li.appendChild(name);
 
-  const date = document.createElement('p');
+  const date = document.createElement('div');
+  date.className = "date";
   date.innerHTML = review.date;
   li.appendChild(date);
 
   const rating = document.createElement('p');
+  rating.className = "rating";
   rating.innerHTML = `Rating: ${review.rating}`;
   li.appendChild(rating);
 
