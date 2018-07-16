0e93094770790ef29b75d6f238bf5bf951f51ff1
brutal-kitten <olgaprudyus@gmail.com>
2018-07-16 16:56:57 +0200

    feat: manage focuse

--- js/main.js
+++ js/main.js
@@ -159,6 +159,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.tabIndex = "1";
   li.append(more)
 
   return li
